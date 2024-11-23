984c3f87c80054b4f88b7756596232ad7e66d375
hajime2 <joaquin.gonzalezparada@gmail.com>
2024-11-23 19:03:19 -0300

    sonido piano ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+const piano = new Tone.Sampler({
+    urls: {
+      A0: 'A0.mp3',
+      C1: 'C1.mp3',
+      'D#1': 'Ds1.mp3',
+      'F#1': 'Fs1.mp3',
+      A1: 'A1.mp3',
+      C2: 'C2.mp3',
+      'D#2': 'Ds2.mp3',
+      'F#2': 'Fs2.mp3',
+      A2: 'A2.mp3',
+      C3: 'C3.mp3',
+      'D#3': 'Ds3.mp3',
+      'F#3': 'Fs3.mp3',
+      A3: 'A3.mp3',
+      C4: 'C4.mp3',
+      'D#4': 'Ds4.mp3',
+      'F#4': 'Fs4.mp3',
+      A4: 'A4.mp3',
+      C5: 'C5.mp3',
+      'D#5': 'Ds5.mp3',
+      'F#5': 'Fs5.mp3',
+      A5: 'A5.mp3',
+      C6: 'C6.mp3',
+      'D#6': 'Ds6.mp3',
+      'F#6': 'Fs6.mp3',
+      A6: 'A6.mp3',
+      C7: 'C7.mp3',
+      'D#7': 'Ds7.mp3',
+      'F#7': 'Fs7.mp3',
+      A7: 'A7.mp3',
+      C8: 'C8.mp3',
+    },
+    release: 1,
+    baseUrl: 'https://tonejs.github.io/audio/salamander/',
+  }).toDestination();
 // Major scales by key
 const majorScales = {
     C: ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
@@ -220,7 +256,7 @@ function playRhythm() {
         combinacion.forEach(({ nota, duracion }, i) => {
             if (nota) {
                 const durationInSeconds = Tone.Time(duracion).toSeconds();
-                synth.triggerAttackRelease(nota, durationInSeconds, time + i * durationInSeconds);
+                piano.triggerAttackRelease(nota, durationInSeconds, time + i * durationInSeconds);
             }
         });
     }, '1m'); // Longitud del loop (1 compás)
@@ -231,3 +267,13 @@ function playRhythm() {
 
     console.log('Ritmo en bucle:', combinacion);
 }
+
+// Función para detener el loop
+document.getElementById('stop-loop').addEventListener('click', function () {
+    if (currentLoopId !== null) {
+        currentLoopId.dispose(); // Detenemos el loop
+        Tone.Transport.stop(); // Detenemos el transporte
+        console.log("Loop detenido.");
+    }
+});
+
